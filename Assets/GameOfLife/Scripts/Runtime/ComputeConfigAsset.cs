37b99482720d1662b195d679bea5bb8b2d938fd4
aillieo <aillieo@163.com>
2023-03-22 23:38:34 +0800

    update

--- Assets/GameOfLife/Scripts/Runtime/ComputeConfigAsset.cs
+++ Assets/GameOfLife/Scripts/Runtime/ComputeConfigAsset.cs
@@ -9,19 +9,7 @@ namespace AillieoGames
     public class ComputeConfigAsset : ScriptableObject
     {
         public ComputeShader computeShader;
-        [SerializeField]
-        public List<int> initState;
 
-        private void OnValidate()
-        {
-            if (this.initState == null || this.initState.Count != 128)
-            {
-                this.initState = new List<int>();
-                for (int i = 0; i < 128; ++i)
-                {
-                    this.initState.Add(i);
-                }
-            }
-        }
+        public Texture2D initStateTexture;
     }
 }
--- Assets/GameOfLife/Scripts/Runtime/ComputePass.cs
+++ Assets/GameOfLife/Scripts/Runtime/ComputePass.cs
@@ -11,7 +11,20 @@ namespace AillieoGames
         private static readonly int targetBufferId = Shader.PropertyToID("TargetBuffer");
         private static readonly int resultId = Shader.PropertyToID("Result");
 
-        private ComputeBuffer computeBuffer;
+        //private ComputeBuffer computeBuffer;
+
+        bool initRT = false;
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            base.Configure(cmd, cameraTextureDescriptor);
+
+            if(!initRT)
+            {
+                cmd.Blit(computeConfig.initStateTexture, targetBufferId);
+                initRT = true;
+            }
+        }
 
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
@@ -32,21 +45,10 @@ namespace AillieoGames
 
             CommandBuffer cmd = CommandBufferPool.Get();
             ScriptableRenderer renderer = renderingData.cameraData.renderer;
-            Camera cam = renderingData.cameraData.camera;
-
-            if (this.computeBuffer == null || !this.computeBuffer.IsValid())
-            {
-                this.computeBuffer = new ComputeBuffer(this.computeConfig.initState.Count, 32);
-            }
-
-            cmd.SetComputeBufferData(this.computeBuffer, this.computeConfig.initState);
 
             ComputeShader computeShader = this.computeConfig.computeShader;
 
             int kernelHandle = computeShader.FindKernel("CSMain");
-
-            cmd.SetComputeBufferParam(computeShader, kernelHandle, "_Tiles", this.computeBuffer);
-            cmd.SetComputeIntParam(computeShader, "_TileCount", this.computeConfig.initState.Count);
 
             cmd.SetComputeTextureParam(computeShader, kernelHandle, resultId, targetBufferId);
             cmd.DispatchCompute(computeShader, kernelHandle, Mathf.CeilToInt(Screen.width / 8), Mathf.CeilToInt(Screen.height / 8), 1);
@@ -70,11 +72,6 @@ namespace AillieoGames
 
         public void Cleanup()
         {
-            if (this.computeBuffer != null && this.computeBuffer.IsValid())
-            {
-                this.computeBuffer.Dispose();
-                this.computeBuffer = null;
-            }
         }
     }
 }
